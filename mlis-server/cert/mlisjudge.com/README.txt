c36c48a7b3d3b2b70eaddc775ca64c5f72a19e1c
Roman Koshlyak <roman@romanpro.local>
2019-09-07 09:42:55 -0700

    Fix build and condition on credentials

---
+++ mlis-server/cert/mlisjudge.com/README.txt
@@ -0,0 +1,1 @@
+Folder for production certificate
--- mlis-server/src/main.ts
+++ mlis-server/src/main.ts
@@ -2,15 +2,35 @@ import fs from 'fs';
 import http from 'http';
 import https from 'https';
 import path from 'path';
+import util from 'util';
 import express from 'express';
 
 import AppServer from './AppServer';
 import initDevData from './initDevData';
 
+const ENCODING = 'utf8';
+
 function sleep(ms:number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function loadCredentials() {
+  const hostname = process.env.HOSTNAME;
+  const keyFileName = `./cert/${hostname}/server.key`;
+  const crtFileName = `./cert/${hostname}/server.crt`;
+
+  const keyFileExist = await util.promisify(fs.exists)(keyFileName);
+  const crtFileExist = await util.promisify(fs.exists)(crtFileName);
+  if (!keyFileExist || !crtFileExist) {
+    console.log('Credentials file not found, https will not be supported');
+    return null;
+  }
+
+  const privateKey  = await fs.promises.readFile(keyFileName, {encoding: ENCODING});
+  const certificate = await fs.promises.readFile(crtFileName, {encoding: ENCODING});
+  const credentials = {key: privateKey, cert: certificate};
+  return credentials;
+}
 async function main() {
   console.log('Init data');
   while (true) {
@@ -27,22 +47,20 @@ async function main() {
 
   const app = express();
   graphqlServer.applyMiddleware({app});
-  console.log(path.join(__dirname, '../client_build'));
   app.use(express.static(path.join(__dirname, '../client_build')));
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../client_build', 'index.html'));
   });
   const httpServer = http.createServer(app);
-  const hostname = process.env.HOSTNAME;
-  const privateKey  = fs.readFileSync(`./cert/${hostname}/server.key`, 'utf8');
-  const certificate = fs.readFileSync(`./cert/${hostname}/server.crt`, 'utf8');
-  const credentials = {key: privateKey, cert: certificate};
-  const httpsServer = https.createServer(credentials, app);
   graphqlServer.installSubscriptionHandlers(httpServer);
-  graphqlServer.installSubscriptionHandlers(httpsServer);
   httpServer.listen(8000);
-  httpsServer.listen(8443);
-  console.log('Server ready.');
+  const credentials = await loadCredentials();
+  if (credentials != null) {
+    const httpsServer = https.createServer(credentials, app);
+    graphqlServer.installSubscriptionHandlers(httpsServer);
+    httpsServer.listen(8443);
+  }
+  console.log('Server started');
 }
 
 main()
