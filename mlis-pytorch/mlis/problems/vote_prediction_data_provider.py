a839c52091bf6faa0c20148fc3059c29192ecbc2
Roman <rkoshlyak@gmail.com>
2019-09-18 00:20:58 -0700

    Speed up data generation for vote prediction

--- mlis-pytorch/mlis/problems/vote_prediction_data_provider.py
+++ mlis-pytorch/mlis/problems/vote_prediction_data_provider.py
@@ -5,34 +5,33 @@
 # We return 1 if sum > voters/2, 0 otherwise
 # We split data in 2 parts, on first part you will train and on second
 # part we will test
+import numpy as np
 import torch
 from ..core.case_data import CaseData
 
 class DataProvider:
-    def get_index(self, tensor_index):
-        index = 0
-        for i in range(tensor_index.size(0)):
-            index = 2*index + tensor_index[i].item()
-        return index
-
-    def calc_value(self, input_data, function_table, input_size, input_count_size):
-        count = 0
-        for i in range(input_count_size):
-            count += function_table[self.get_index(input_data[i*input_size: (i+1)*input_size])].item()
-        if count > input_count_size/2:
-            return 1
-        else:
-            return 0
+    def packbits(self, x, bits_count):
+        row_count = x.size(0)
+        x = x.view(-1, bits_count)
+        x = x.byte().numpy()
+        x = np.packbits(x, axis=-1)
+        x = torch.from_numpy(x).long()
+        x = x.view(row_count, -1)
+        return x
 
     def create_data(self, data_size, input_size, input_count_size, seed):
         torch.manual_seed(seed)
         function_size = 1 << input_size
         function_table = torch.ByteTensor(function_size).random_(0, 2)
+        function_table2 = reversed(function_table)
         total_input_size = input_size*input_count_size
         data = torch.ByteTensor(data_size, total_input_size).random_(0, 2)
         target = torch.ByteTensor(data_size)
-        for i in range(data_size):
-            target[i] = self.calc_value(data[i], function_table, input_size, input_count_size)
+        ind_data = self.packbits(data, input_size)
+        function_data = function_table[ind_data]
+        sum_data = function_data.sum(dim=1)
+        target = sum_data > input_count_size/2
+
         return (data.float(), target.view(-1, 1).float())
 
     def create_case_data(self, case):
