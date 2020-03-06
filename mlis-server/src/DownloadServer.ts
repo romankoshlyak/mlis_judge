74f3f43ee35a215e8505a4afff009953270f30f2
Roman <rkoshlyak@gmail.com>
2020-03-06 01:49:31 -0700

    Fix bugs. remove update code

--- mlis-server/src/DownloadServer.ts
+++ mlis-server/src/DownloadServer.ts
@@ -6,7 +6,7 @@ import { Express } from "express-serve-static-core";
 
 import AppContext from './context';
 import models, { Test, Problem, Submission } from './models';
-import { getModelId, requireValue, assertTrue } from './utils';
+import { requireValue, assertTrue } from './utils';
 
 const ENCODING = 'utf8';
 const MLIS_DIR_NAME = '/usr/src/mlis-pytorch/mlis/';
@@ -36,21 +36,7 @@ export default class DownloadServer {
     });
   }
 
-  private async fixProblemTemplate(context: AppContext) {
-    const problemId = getModelId("UHJvYmxlbToy", Problem);
-    const problem = requireValue(await Problem.findByPk(problemId));
-    if (problem.name == "Hello Xor") {
-      let notebookTemplate  = await fs.promises.readFile(`${MLIS_DIR_NAME}problems/hello_xor_code_template.py`, {encoding: ENCODING});
-      console.log("before_check");
-      if (problem.codeTemplate != notebookTemplate) {
-        console.log("update");
-        problem.codeTemplate = notebookTemplate;
-        await problem.save();
-      }
-    }
-  }
   private async getNotebook(context: AppContext, id: string): Promise<[string, string]> {
-    await this.fixProblemTemplate(context);
     let notebookTemplate  = await fs.promises.readFile("./templates/notebook.ipynb_template", {encoding: ENCODING});
     let runGridSearch  = await fs.promises.readFile(MLIS_RUN_GRID_SEARCH, {encoding: ENCODING});
     const mlisCoreCodebase = await this.getCodeFromDir(MLIS_CORE_DIR_NAME);
